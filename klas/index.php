af56d73f2357ba78398e43571941b97201a06add
mno-davinci <mnouwen@davinci.nl>
2015-03-27 11:04:14 +0100

    Laatste update voor vrijdag

--- klas/index.php
+++ klas/index.php
@@ -10,7 +10,7 @@ switch ($action) {
 			1. We gaan een formulier tonen.
 			2. Formulier gegevens opslaan in database.
 		*/
-
+			echo $_GET['id'] . $_GET['action'];
 		break;
 
 	case 'read':
@@ -20,7 +20,7 @@ switch ($action) {
 			1. Gegevens uitlezen uit database (ID nodig!)
 			2. Tonen gegevens uit de database.
 		*/
-
+			echo $_GET['id'] . $_GET['action'];
 		break;	
 	
 	case 'update':
@@ -31,7 +31,7 @@ switch ($action) {
 			2. Tonen gegevens database in formulier.
 			3. Formulier gegevens updaten in database.
 		*/
-
+			echo $_GET['id'] . $_GET['action'];
 		break;
 
 	case 'delete':
@@ -42,10 +42,10 @@ switch ($action) {
 			2. Gegevens tonen in beeld, bevestiging vragen
 			3. Gegevens verwijderen, indien bevestigd! Anders terug naar default.
 		*/
-
+			echo $_GET['id'] . $_GET['action'];
 		break;
 
 	default:
-		
+			require("view/default_table.php");
 		break;
 }
---
+++ klas/view/default_table.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Klas APP</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<table border="1">
+		<tr>
+			<th>ID</th>
+			<th>Voornaam</th>
+			<th>Achternaam</th>
+			<th colspan="3">Actie</th>
+		</tr>
+
+		<tr>
+			
+			<td>2</td>
+			<td>Maarten</td>
+			<td>Nouwen</td>
+			<td><a href="index.php?action=read&id=2">Read</a></td>
+			<td><a href="index.php?action=update&id=2">Update</a></td>
+			<td><a href="index.php?action=delete&id=2">Delete</a></td>
+
+		</tr>
+
+		<tr>
+			
+			<td>3</td>
+			<td>Jason</td>
+			<td>Roffel</td>
+			<td><a href="index.php?action=read&id=3">Read</a></td>
+			<td><a href="index.php?action=update&id=3">Update</a></td>
+			<td><a href="index.php?action=delete&id=3">Delete</a></td>
+
+		</tr>
+	</table>
+</body>
+</html>
